dc96360d1cfcce59a2d2a3975bd16a47e5c99cb1
zach brecht <brechtz32@gmail.com>
2024-01-29 21:22:33 -0600

    first commit of Operato Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine($"What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {AreaOfCircle(radius)}");
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+
+        
     }
 }
